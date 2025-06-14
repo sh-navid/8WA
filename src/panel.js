68a5fb37ef4e33df00e5c182d76d0eb51fb05662
sh-navid <shamshirsaz.navid@live.com>
2025-06-14 09:16:36 +0330

    [Revision] 6d1ac7b

--- src/panel.js
+++ src/panel.js
@@ -14,3 +14,90 @@ function clearChat() {
   ];
   chatMessages.html("");
 }
+
+function highlightCode(codeElement, code) {
+  Prism.highlightElement(codeElement[0]);
+}
+
+function addMessage(text, fromUser = true) {
+  const msgDiv = $("<div>")
+    .addClass("message")
+    .addClass(fromUser ? "user" : "bot");
+
+  if (text.length > 150 && fromUser) {
+    const shortText = text.substring(0, 150);
+    const remainingText = text.substring(150);
+
+    const shortSpan = $("<span>").text(shortText);
+    const moreButton = $("<button>").text("More...").addClass("more-button");
+    const fullSpan = $("<span>").text(remainingText).hide();
+
+    moreButton.click(() => {
+      fullSpan.show();
+      moreButton.hide();
+    });
+
+    msgDiv.append(shortSpan, moreButton, fullSpan);
+  } else {
+    msgDiv.text(text);
+  }
+
+  chatMessages.append(msgDiv).scrollTop(chatMessages[0].scrollHeight);
+}
+
+function addBotMessage(response) {
+  const msgDiv = $("<div>").addClass("message bot");
+  if (response?.choices?.[0]?.message?.content) {
+    let content = response.choices[0].message.content;
+
+    const markedContent = marked.parse(content);
+
+    msgDiv.html(markedContent);
+    chatMessages.append(msgDiv).scrollTop(chatMessages[0].scrollHeight);
+
+    msgDiv.find("pre code").each(function () {
+      const codeElement = $(this);
+      const code = codeElement.text();
+
+      highlightCode(codeElement, code);
+
+      codeElement.parent().css("position", "relative").append(`
+              <div class="code-btns-container">
+                  <img src="${btnOpenCodeFile}" alt="Open Code File"  />
+                  <img src="${btnAppend}"       alt="Append in File"  />
+                  <img src="${btnReplace}"      alt="Replace File"    />
+                  <img src="${btnCopy}"         alt="Copy File"       />
+              </div>
+            `);
+
+      codeElement
+        .parent()
+        .find(".code-btns-container img:eq(0)")
+        .click(() => {
+          vscode.postMessage({ command: "openCodeFile", code });
+        });
+      codeElement
+        .parent()
+        .find(".code-btns-container img:eq(1)")
+        .click(() => {
+          vscode.postMessage({ command: "appendToActiveFile", code });
+        });
+      codeElement
+        .parent()
+        .find(".code-btns-container img:eq(2)")
+        .click(() => {
+          vscode.postMessage({ command: "replaceActiveFile", code });
+        });
+      codeElement
+        .parent()
+        .find(".code-btns-container img:eq(3)")
+        .click(() => {
+          vscode.postMessage({ command: "copyCodeBlock", code });
+        });
+    });
+    msgArray.push({ role: "assistant", content: markedContent });
+  } else {
+    msgDiv.text("No response from bot.");
+    chatMessages.append(msgDiv).scrollTop(chatMessages[0].scrollHeight);
+  }
+}
