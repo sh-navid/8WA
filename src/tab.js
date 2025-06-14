2c2a45ad7ecdeb86c4cfa9fac82fb54c746b576d
sh-navid <shamshirsaz.navid@live.com>
2025-06-14 08:37:29 +0330

    [Enhancement] Fixed functionality of active tab

--- src/tab.js
+++ src/tab.js
@@ -6,6 +6,14 @@ window.onload = function () {
   tabs = $("tabs");
   addBtn = tabs.find(".addTab");
   addBtn.on("click", createTab);
+
+  // Add click event listeners to existing tabs
+  tabs.find("tab").on("click", function() {
+    setActiveTab($(this));
+  });
+
+  // Initialize active tab (set first tab as active by default)
+  setActiveTab(tabs.find("tab:first-child"));
 };
 
 function createTab() {
@@ -15,5 +23,20 @@ function createTab() {
   newTab.find(".remTab").on("click", () => {
     newTab.remove();
   });
+  newTab.on("click", () => {
+    setActiveTab(newTab);
+  });
   tabs.append(newTab);
+}
+
+function setActiveTab(tab) {
+  // Remove active class from all tabs
+  tabs.find("tab").removeClass("active");
+
+  // Set active class on the clicked tab
+  tab.addClass("active");
+
+  // Add functionality to load content for each tab here
+  // based on which tab is active
+  // For example: loadTabContent(tab.find(".tabTitle").html());
 }
