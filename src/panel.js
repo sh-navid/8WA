50eb1f87d735cfca68cf7cec74d3b77880c9d3fb
sh-navid <shamshirsaz.navid@live.com>
2025-07-24 07:42:35 +0330

    [Enhancement] Removed tab.js to reconstruct the structure later

--- src/panel.js
+++ src/panel.js
@@ -462,3 +462,36 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+
+
+
+
+
+
+let tabs;
+let addBtn;
+let tabCount = 1;
+
+window.onload = function () {
+  tabs = $("tabs");
+  addBtn = tabs.find(".addTab");
+  addBtn.on("click", createTab);
+  tabs.find("tab").on("click", function() {
+    setActiveTab($(this));
+  });
+  setActiveTab(tabs.find("tab:first-child"));
+};
+
+function createTab() {
+  const newTab = $($(".tabTemplate").html());
+  newTab.find(".tabTitle").html("Chat " + ++tabCount);
+  newTab.find(".remTab").on("click", () => newTab.remove());
+  newTab.on("click", () => setActiveTab(newTab));
+  tabs.append(newTab);
+}
+
+function setActiveTab(tab) {
+  tabs.find("tab").removeClass("active");
+  tab.addClass("active");
+}
