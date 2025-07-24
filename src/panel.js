8baf75d67390351cbc39a498dfb5020d69e2d6c4
sh-navid <shamshirsaz.navid@live.com>
2025-07-24 08:02:38 +0330

    [Enhancement] Extracted tabManager from panel

--- src/panel.js
+++ src/panel.js
@@ -1,3 +1,6 @@
+/*[[src/panel.js]]*/
+import TabManager from './components/tabManager.js';
+
 let msgArray = [
   {
     role: "assistant",
@@ -461,37 +464,6 @@ document.addEventListener("DOMContentLoaded", function () {
       hideCommandPanel();
     }
   });
+
+  new TabManager("#tabs", ".addTab", ".tabTemplate");
 });
-
-
-
-
-
-
-
-let tabs;
-let addBtn;
-let tabCount = 1;
-
-window.onload = function () {
-  tabs = $("tabs");
-  addBtn = tabs.find(".addTab");
-  addBtn.on("click", createTab);
-  tabs.find("tab").on("click", function() {
-    setActiveTab($(this));
-  });
-  setActiveTab(tabs.find("tab:first-child"));
-};
-
-function createTab() {
-  const newTab = $($(".tabTemplate").html());
-  newTab.find(".tabTitle").html("Chat " + ++tabCount);
-  newTab.find(".remTab").on("click", () => newTab.remove());
-  newTab.on("click", () => setActiveTab(newTab));
-  tabs.append(newTab);
-}
-
-function setActiveTab(tab) {
-  tabs.find("tab").removeClass("active");
-  tab.addClass("active");
-}
