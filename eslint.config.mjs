56f3be208b868566d55118b4b393c7a23ce94cfa
sh-navid <shamshirsaz.navid@live.com>
2025-09-01 18:48:08 +0330

    [Enhnacement] Cleared the config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,25 +5,24 @@ export default [{
     languageOptions: {
         globals: {
             ...globals.commonjs,
-            ...globals.node,
             ...globals.mocha,
+            ...globals.node,
+            document: "readonly", 
+            window: "readonly",
+            vscode: "readonly", 
             $: "readonly", // This line tells ESLint that '$' is a global variable
-            window: "readonly", // This line tells ESLint that 'window' is a global variable
-            document: "readonly", // This line tells ESLint that 'document' is a global variable
-            vscode: "readonly", // This line tells ESLint that 'vscode' is a global variable
         },
-
         ecmaVersion: 2022,
         sourceType: "module",
     },
 
     rules: {
-        "no-const-assign": "warn",
         "no-this-before-super": "warn",
-        "no-undef": "warn",
+        "constructor-super": "warn",
+        "no-const-assign": "warn",
         "no-unreachable": "warn",
         "no-unused-vars": "warn",
-        "constructor-super": "warn",
         "valid-typeof": "warn",
+        "no-undef": "warn",
     },
 }];
