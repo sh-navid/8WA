53678cb13e63efa888ca025e999cda3f9f873582
sh-navid <shamshirsaz.navid@live.com>
2025-09-01 09:28:08 +0330

    [Bug] #99 Fix the removeCommentStructure problem

--- src/helpers/codeHelper.test.js
+++ src/helpers/codeHelper.test.js
@@ -1,7 +1,7 @@
+const { removeCommentStructure } = require('./codeHelper');
 const assert = require('assert');
-const { removeCommentStructure } = require('../../src/helpers/codeHelper');
 
-suite('codeHelper Tests', function () {
+describe('codeHelper Tests', function () {
   test('removeCommentStructure should handle different types of comments', function () {
     const code1 = '// [[src/index.js]]';
     const code2 = '# [[src/index.py]]';
@@ -10,6 +10,10 @@ suite('codeHelper Tests', function () {
     const code5 = "'''[[index.py]]'''";
     const code6 = '/* [[src/utils/myUtil.js]] */';
     const code7 = '<!-- [[src/index.html]] -->';
+    const code8 = 'const x = 1; // [[src/index.js]]';
+    const code9 = 'const x = 1;/* [[src/utils/myUtil.js]] */';
+    const code10 = 'const x = 1;/** [[src/utils/myUtil.js]] **/';
+    const code11 = '<!--[[src/index.html]]-->';
 
     assert.strictEqual(removeCommentStructure(code1), '');
     assert.strictEqual(removeCommentStructure(code2), '');
@@ -18,5 +22,9 @@ suite('codeHelper Tests', function () {
     assert.strictEqual(removeCommentStructure(code5), '');
     assert.strictEqual(removeCommentStructure(code6), '');
     assert.strictEqual(removeCommentStructure(code7), '');
+    assert.strictEqual(removeCommentStructure(code8), 'const x = 1;');
+    assert.strictEqual(removeCommentStructure(code9), 'const x = 1;');
+    assert.strictEqual(removeCommentStructure(code10), 'const x = 1;');
+    assert.strictEqual(removeCommentStructure(code11), '');
   });
 });
