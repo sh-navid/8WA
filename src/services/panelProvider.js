afb199b7219a5ec3a923d8fb011a68c158054960
sh-navid <shamshirsaz.navid@live.com>
2025-09-04 20:32:42 +0330

    [Enhancement] #75 Clean structure

--- src/services/panelProvider.js
+++ src/services/panelProvider.js
@@ -1,86 +1,76 @@
-const { removeCommentStructure } = require("../helpers/codeHelper");
-const { uri, load } = require("../helpers/fileSystemHelper");
-const config = require("./../../configs/config.json");
-const { callGitDiscard } = require("./gitService");
-const FileService = require("./fileService");
-const vscode = require("vscode");
-const {
-  openCodeFile,
-  copyCodeBlock,
-  sendToTerminal,
-} = require("../commands/commands");
+const {removeCommentStructure} = require("../helpers/codeHelper")
+const {uri, load} = require("../helpers/fileSystemHelper")
+const config = require("./../../configs/config.json")
+const {callGitDiscard} = require("./gitService")
+const FileService = require("./fileService")
+const vscode = require("vscode")
+const {openCodeFile, copyCodeBlock, sendToTerminal} = require("../commands/commands")
 const {
   undoCodeBlock,
   diffCodeBlock,
   buildProjectStructure,
-} = require("../helpers/extensionHelper");
+} = require("../helpers/extensionHelper")
 
 class PanelProvider {
   constructor(extensionUri) {
-    this._extensionUri = extensionUri;
-    this._backupFilePath = null;
-    this._view = null;
-    this._fileService = new FileService();
+    this._extensionUri = extensionUri
+    this._backupFilePath = null
+    this._view = null
+    this._fileService = new FileService()
   }
 
   async resolveWebviewView(webviewView) {
-    this._view = webviewView;
+    this._view = webviewView
     webviewView.webview.options = {
       enableScripts: true,
       localResourceRoots: [this._extensionUri],
-    };
-    webviewView.webview.onDidReceiveMessage(
-      this._handleMessage.bind(this, webviewView)
-    );
-    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+    }
+    webviewView.webview.onDidReceiveMessage(this._handleMessage.bind(this, webviewView))
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview)
   }
 
   async _handleMessage(webviewView, message) {
     switch (message.command) {
       case "openCodeFile":
-        await openCodeFile(message.code);
-        break;
+        await openCodeFile(message.code)
+        break
       case "replaceActiveFile":
-        await openCodeFile(message.code);
-        await this._fileService.replaceActiveFile(message.code);
-        break;
+        await openCodeFile(message.code)
+        await this._fileService.replaceActiveFile(message.code)
+        break
       case "replaceCodeFileSilently":
-        await this._fileService.replaceCodeFileSilently(message.code);
-        break;
+        await this._fileService.replaceCodeFileSilently(message.code)
+        break
       case "copyCodeBlock":
-        await copyCodeBlock(removeCommentStructure(message.code));
-        break;
+        await copyCodeBlock(removeCommentStructure(message.code))
+        break
       case "buildProjectStructure":
-        await buildProjectStructure(webviewView);
-        break;
+        await buildProjectStructure(webviewView)
+        break
       case "diffCodeBlock":
-        await diffCodeBlock(message.code);
-        break;
+        await diffCodeBlock(message.code)
+        break
       case "undoCodeBlock":
-        await undoCodeBlock();
-        break;
+        await undoCodeBlock()
+        break
       case "callGitDiscard":
-        await callGitDiscard();
-        break;
+        await callGitDiscard()
+        break
       case "sendToTerminal":
-        await sendToTerminal(message.code);
-        break;
+        await sendToTerminal(message.code)
+        break
     }
   }
 
   _getHtmlForWebview(webview) {
-    let html = load(this, "views", "panel.html");
+    let html = load(this, "views", "panel.html")
+    const vsConfig = vscode.workspace.getConfiguration("nabotx")
     const _config = {
-      path: "",
-      token: "",
-      model: "",
-      previewUrl: "http://localhost:3000",
-    };
-    const vsConfig = vscode.workspace.getConfiguration("nabotx");
-    const pathValue = vsConfig.get("path") || _config.path;
-    const tokenValue = vsConfig.get("token") || _config.token;
-    const modelValue = vsConfig.get("model") || _config.model;
-    const previewUrlValue = vsConfig.get("previewUrl") || _config.previewUrl;
+      previewUrl: vsConfig.get("previewUrl") || "http://localhost:3000",
+      token: vsConfig.get("token") || "",
+      model: vsConfig.get("model") || "",
+      path: vsConfig.get("path") || "",
+    }
 
     const scripts = config.scripts
       .map(
@@ -89,7 +79,7 @@ class PanelProvider {
             x.startsWith("~/") ? uri(webview, this, "src", x.slice(2)) : x
           }"></script>`
       )
-      .join("");
+      .join("")
     const styles = config.styles
       .map(
         (x) =>
@@ -97,26 +87,26 @@ class PanelProvider {
             x.startsWith("~/") ? uri(webview, this, "styles", x.slice(2)) : x
           }" rel="stylesheet"/>`
       )
-      .join("");
+      .join("")
 
     html = html
       .replaceAll(/\$\{rules\}/g, config.rules.assistant)
-      .replaceAll(/\$\{previewUrl\}/g, previewUrlValue)
-      .replaceAll(/\$\{token\}/g, tokenValue)
-      .replaceAll(/\$\{model\}/g, modelValue)
+      .replaceAll(/\$\{previewUrl\}/g, _config.previewUrl)
+      .replaceAll(/\$\{token\}/g, _config.token)
+      .replaceAll(/\$\{model\}/g, _config.model)
+      .replaceAll(/\$\{path\}/g, _config.path)
       .replaceAll(/\$\{scripts\}/g, scripts)
-      .replaceAll(/\$\{path\}/g, pathValue)
-      .replaceAll(/\$\{styles\}/g, styles);
+      .replaceAll(/\$\{styles\}/g, styles)
 
     for (const asset of config.assets) {
       html = html.replaceAll(
         new RegExp(`\\$\\{${asset.slice(2)}\\}`, "g"),
         uri(webview, this, "assets", asset.slice(2))
-      );
+      )
     }
 
-    return html;
+    return html
   }
 }
 
-module.exports = PanelProvider;
+module.exports = PanelProvider
